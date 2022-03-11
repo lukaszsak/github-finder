2873c1aa8fc6f14eea3cda059406f008f551c1a3
Łukasz Sak <lukasz.sak@gmail.com>
2022-03-11 14:33:38 +0100

    UserItem and Navbar changed to funcion components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
+const UserItem = ({user:{login, avatar_url, html_url}}) => {
+    return (
+        <div className='card text-center'>
+            <img 
+                src={avatar_url} 
+                alt='' 
+                className='round-img' 
+                style={{ width: '60px'}}
+            />
+            <h3>{login}</h3>
 
-    render() {
-        const { login,avatar_url,html_url } = this.props.user
-
-        return (
-            <div className='card text-center'>
-                <img 
-                    src={avatar_url} 
-                    alt='' 
-                    className='round-img' 
-                    style={{ width: '60px'}}
-                />
-                <h3>{login}</h3>
-
-                <div>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-                </div>
+            <div>
+                <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
             </div>
-        )
-    }
+        </div>
+    )
+}
+
+UserItem.protoTypes = {
+    user: PropTypes.object.isRequired
 }
 
 export default UserItem
