349967a05c67ae4d0a453c62647d322a30f99600
Łukasz Sak <lukasz.sak@gmail.com>
2022-03-11 20:36:18 +0100

    axios api calls, .env variables, spinner

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react'
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users'
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: true
+  }
+
+  async componentDidMount(){
+
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState({users:res.data, loading: false})
+
+    // console.log(res.data)
+  }
 
   render(){
     const name = 'plum'
@@ -11,7 +24,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,10 @@
+import { Fragment } from 'react'
+import spinner from './spinner.gif';
+
+const Spinner = () =>  (
+    <>
+        <img src={spinner} alt="loading..." />
+    </>
+  )
+
+export default Spinner
