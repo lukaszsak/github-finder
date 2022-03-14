d75a777e3fdc9bbd17b0a7080d28b7fcd2e92aba
Łukasz Sak <lukasz.sak@gmail.com>
2022-03-14 13:25:44 +0100

    User component added - displays info about specific Github user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users'
 import Search from './components/users/Search';
 import axios from 'axios';
 import { Alert } from './components/layout/Alert';
+import About from './components/pages/About';
+import User from './components/users/User';
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   }
@@ -25,6 +29,22 @@ class App extends Component {
     this.setState({users:res.data.items, loading: false})
   }
 
+  // get single Github user
+  getUser = async username => {
+    this.setState({loading: true})
+    console.log('username ::',username)
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?
+      client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+
+    console.log(res)
+    
+    this.setState({user:res.data, loading: false})
+  
+    
+  }
+
   // clears users from state
   clearUsers = () => this.setState({users:[], loading: false})
 
@@ -34,17 +54,32 @@ class App extends Component {
   }
 
   render(){
-    const { users, loading, alert } = this.state
+    const { users, loading, alert, user } = this.state
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={alert}/>
-          <Search searchUsers={this.searchUsers} setAlert={this.setAlert}/>
-          { users.length != 0 && <button className="btn btn-light btn-block" onClick={this.clearUsers}>Clear</button> }
-          <Users loading={loading} users={users} />
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={alert}/>
+
+            <Routes>
+              <Route 
+                path='/' 
+                element={(
+                <Fragment>
+                  <Search searchUsers={this.searchUsers} setAlert={this.setAlert}/>
+                  { users.length != 0 && <button className="btn btn-light btn-block" onClick={this.clearUsers}>Clear</button> }
+                  <Users loading={loading} users={users} />
+                </Fragment>
+                )}
+              /> 
+              <Route path='/about' element={<About/>} />
+              <Route path='/user/:login' element={<User getUser={this.getUser} user={user} loading={loading}/>} />
+            </Routes>
+
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+  return (
+    <Fragment>
+        <h1>About this app</h1>
+        <p>app to search GitHub users</p>
+        <p>version 1.0.0</p>
+    </Fragment>
+  )
+}
+
+export default About
